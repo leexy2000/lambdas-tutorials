5b5be1b1908b5ffcd31479c9f9fcfcf10fbb23a4
leexy2000 <leexy2000@hotmail.com>
2018-09-29 17:02:33 +0800

    anonymous class

---
+++ src/main/java/org/tutorialspoint/section04/CheckIf.java
@@ -0,0 +1,7 @@
+package org.tutorialspoint.section04;
+
+public interface CheckIf {
+
+	boolean test(String str);
+
+}
---
+++ src/main/java/org/tutorialspoint/section04/StringTest.java
@@ -0,0 +1,37 @@
+package org.tutorialspoint.section04;
+
+import java.util.*;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+
+		List<String> arr = new ArrayList<>();
+		arr.add("Git is a VCS");
+		arr.add("Git is a distributed VCS");
+		arr.add("Git is free");
+		arr.add("Git is free under the GPL");
+		arr.add("I love Git");
+		arr.add("I have been studying Git for a long time.");
+		arr.add("But I'm still not good at it");
+
+		// anonymous class implement
+		printString(arr, 
+				new CheckIf() {
+					public boolean test(String str) {
+						return str.contains("Git") && str.length() > 20;
+					}
+				}
+		);
+		
+	}
+
+	// Approach 4: Specify Search Criteria Code in anonymous class
+	private static void printString(List<String> list, CheckIf checker) {
+		for (String str : list) {
+			if (checker.test(str))
+				System.out.println(str);
+		}
+	}
+
+}
